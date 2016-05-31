14b16e8a8183e97290b67d3cf6f47792a6c58fdf
Federico Leva <federicoleva@tiscali.it>
2016-05-31 22:46:37 +0200

    Temporary uploader within iccd-trc2csv.py, CSV export is broken
    Also complete the conversion of three-letter repeatable fields such as MTC.

--- iccd-trc2csv.py
+++ iccd-trc2csv.py
@@ -17,6 +17,12 @@ from collections import namedtuple, defaultdict
 import os
 import re
 import sys
+import subprocess
+# Uploader
+import pywikibot
+import pywikibot.data.api
+from pywikibot import config
+from upload import UploadRobot
 
 # Campi ripetibili
 # http://www.iccd.beniculturali.it/index.php?it/473/standard-catalografici/Standard/29
@@ -30,7 +36,7 @@ data = []
 for i in range(0, len(records)-1):
 	data.append({})
 	counter = defaultdict(int)
-	record = re.sub(r'\n {6}', '', records[i])
+	record = re.sub(r'\n {6}', '', re.sub(r'\.\n {6}', ' ', records[i]))
 	for field in record.splitlines():
 		datum = field.split(': ', 1)
 		if len(datum) < 2:
@@ -40,6 +46,10 @@ for i in range(0, len(records)-1):
 			if datum[0] in rip:
 				counter[datum[0]] += 1
 			continue
+		else:
+			# Take note of which iteration of this field we're at, to properly store subfields.
+			if datum[0] in rip:
+				counter[datum[0]] += 1
 		if datum[0] not in rip:
 			if datum[0][:-1] not in rip:
 				# We're in luck! Just add the field to our table.
@@ -48,10 +58,54 @@ for i in range(0, len(records)-1):
 				data[i][datum[0][:-1] + str(counter[datum[0][:-1]]) + datum[0][-1]] = datum[1]
 		else:
 			if datum[0][:-1] not in rip:
-				data[i][datum[0] + '1'] = datum[1]
+				data[i][datum[0] + str(counter[datum[0]])] = datum[1]
 			else:
-				data[i][datum[0][:-1] + str(counter[datum[0][:-1]]) + datum[0][-1] + '1'] = datum[1]
+				data[i][datum[0][:-1] + str(counter[datum[0][:-1]]) + datum[0][-1] + str(counter[datum[0]])] = datum[1]
 
+# Anticipate the upload here until we get the CSV writing fixed
+# FIXME: split and actually save to csvfile
+
+for i in range(0, len(data)-1):
+	description = u"""{{ICCD TRC
+| institution = {{institution:Museoscienza}}
+| permission =  {{cc-by-sa-4.0}}
+"""
+	for key in data[i].iterkeys():
+		if key == "IDK":
+			description += "| source = {{Museoscienza|idk=%s}}\n" % data[i]['IDK']
+		else:
+			description += u"| %s = %s\n" % (key, data[i][key])
+	description += u"}}"
+
+	filenames = []
+	# The filenames may have excess leading zeros, but we do not want partial matches.
+	needle = r'(^|[^0-9])0*%s[^0-9]' % re.sub('[^0-9]', '', data[i]['INV1N'])
+	directory = './Foto_CATALOGO_01/%s_foto/' % data[i]['IDK'].split('-')[0].strip()
+	for image in os.listdir(directory):
+		if re.match(needle, image):
+			filenames.append(directory + image)
+	if not filenames:
+		print "ERROR: No files found for record %s, inventory %s" % (data[i]['IDK'], data[i]['INV1N'])
+		continue
+
+	for filename in filenames:
+		try:
+			prefix = "%s %s" % (data[i]['OGTD'], data[i]['OGTT'])
+		except:
+			prefix = data[i]['OGTD']
+		prefix = re.sub('[#<>\[\]|{}/?]', '', prefix)
+		commons = u"%s - Museo scienza tecnologia Milano %s" % (prefix, filename.split('/')[-1])
+		print commons
+		try:
+			upload = UploadRobot(filename, description=description,
+								useFilename=commons, keepFilename=True,
+								verifyDescription=False, ignoreWarning=False, aborts=True)
+			upload.run()
+			os.remove(filename)
+		except:
+			pywikibot.output("ERROR: The upload could not be completed.")
+
+"""
 # Prepare to write out to CSV: find out what columns we need
 fieldnames = {}
 header = []
@@ -73,8 +127,9 @@ table = [table._make(row) for row in data]
 # Actually write out to CSV
 with codecs.open('iccd.csv', 'w', encoding='utf-8') as csvfile:
 	out = csv.writer(csvfile, delimiter='\t',
-				  lineterminator='\n',
-				  quoting=csv.QUOTE_MINIMAL)
+				lineterminator='\n',
+				quoting=csv.QUOTE_MINIMAL)
 	out.writerow(header)
 	for row in table:
 		out.writerow(row)
+"""
