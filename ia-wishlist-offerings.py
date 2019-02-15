9b898c2c5baf7619a908c6334d904f1a9096e04e
Federico Leva <federicoleva@tiscali.it>
2019-02-16 00:57:39 +0200

    Split to function

--- ia-wishlist-offerings.py
+++ ia-wishlist-offerings.py
@@ -20,12 +20,29 @@ wishlist = open('wishlist.txt', 'rb')
 offerings = open('wishlist-offerings.csv', 'a')
 
 writer = csv.writer(offerings,
-		delimiter=b',',
+		delimiter=b'\t',
 		lineterminator='\n',
 		quoting=csv.QUOTE_MINIMAL,
 		encoding='utf-8'
 		)
 writer.writerow([u'ISBN', u'Bookshop', u'Price', u'Title', u'Year', 'Notes'])
+
+def printAbebooks(isbn):
+	listing = html.fromstring( t.get('http://www.abebooks.it/servlet/SearchResults?isbn=%s&sortby=2&n=200000237' % code , timeout=10).text )
+	price = listing.xpath('//div[@id="srp-item-price-1"]/text()')[0]
+	seller = listing.xpath('//a[text()="Informazioni sul venditore"]/@href')[0]
+	title = listing.xpath('//div[@id="book-1"]//h2/a[@itemprop="url"]/@title')[0]
+	description = listing.xpath('//div[@id="book-1"]//p[contains(@class, "p-md-t")]/span[contains(.," 19") or contains(.," 20")]/text()')
+	if description:
+		year = re.findall(r'\b(?:19|20)[0-9]{2}\b', description[0])[0]
+	if 'Anybook' in seller:
+		notes = listing.xpath('//div[@id="book-1"]//p[contains(@class, "p-md-t")]/text()')[0].strip().replace('Codice articolo ', '')
+	writer.writerow([code, seller, price, title, year, notes])
+
+def printLibraccio(isbn):
+	listing = html.fromstring( s.get('http://libraccio.it/libro/%s/' % code ).text )
+	price = listing.xpath('//span[@class="currentprice" and @id="C_C_ProductDetail_lSellPriceU"]/text()')[0]
+	writer.writerow([code, u'Libraccio', price, u''])
 
 for code in wishlist.readlines():
 	code = code.strip()
@@ -33,20 +50,7 @@ for code in wishlist.readlines():
 	notes = ''
 	year = ''
 	try:
-	#	listing = html.fromstring( s.get('http://libraccio.it/libro/%s/' % code ).text )
-	#	price = listing.xpath('//span[@class="currentprice" and @id="C_C_ProductDetail_lSellPriceU"]/text()')[0]
-	#	writer.writerow([code, u'Libraccio', price, u''])
-
-		listing = html.fromstring( t.get('http://www.abebooks.it/servlet/SearchResults?isbn=%s&sortby=2' % code , timeout=10).text )
-		price = listing.xpath('//div[@id="srp-item-price-1"]/text()')[0]
-		seller = listing.xpath('//a[text()="Informazioni sul venditore"]/@href')[0]
-		title = listing.xpath('//div[@id="book-1"]//h2/a[@itemprop="url"]/@title')[0]
-		description = listing.xpath('//div[@id="book-1"]//p[contains(@class, "p-md-t")]/span[contains(.," 19") or contains(.," 20")]/text()')
-		if description:
-			year = re.findall(r'\b(?:19|20)[0-9]{2}\b', description[0])[0]
-		if 'Anybook' in seller:
-			notes = listing.xpath('//div[@id="book-1"]//p[contains(@class, "p-md-t")]/text()')[0].strip().replace('Codice articolo ', '')
-		writer.writerow([code, seller, price, title, year, notes])
+		printAbebooks(code)
 	except IndexError:
 		print("NOTICE: not found")
 	except requests.exceptions.ConnectionError:
