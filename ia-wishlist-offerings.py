9c2e1265e151b25b8dc251ff3d86320f2bf2fe89
Federico Leva <federicoleva@tiscali.it>
2019-03-08 11:07:48 +0200

    Refactor and add alternatives to IA wishlist queries

--- ia-wishlist-offerings.py
+++ ia-wishlist-offerings.py
@@ -6,62 +6,83 @@
 #
 # Distributed under the terms of the MIT license.
 #
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 import requests
 from lxml import html
 import re
 from time import sleep
 import unicodecsv as csv
-
 s = requests.Session()
 t = requests.Session()
-wishlist = open('wishlist.txt', 'rb')
-offerings = open('wishlist-offerings.csv', 'a')
 
-writer = csv.writer(offerings,
-		delimiter=b'\t',
-		lineterminator='\n',
-		quoting=csv.QUOTE_MINIMAL,
-		encoding='utf-8'
-		)
-writer.writerow([u'ISBN', u'Bookshop', u'Price', u'Title', u'Year', 'Notes'])
-
-def printAbebooks(isbn):
-	listing = html.fromstring( t.get('http://www.abebooks.it/servlet/SearchResults?isbn=%s&sortby=2&n=200000237' % code , timeout=10).text )
+def getAbebooks(isbn):
+	listing = html.fromstring( t.get('http://www.abebooks.it/servlet/SearchResults?isbn=%s&sortby=2&n=200000237' % isbn , timeout=10).text )
 	price = listing.xpath('//div[@id="srp-item-price-1"]/text()')[0]
-	seller = listing.xpath('//a[text()="Informazioni sul venditore"]/@href')[0]
-	title = listing.xpath('//div[@id="book-1"]//h2/a[@itemprop="url"]/@title')[0]
-	description = listing.xpath('//div[@id="book-1"]//p[contains(@class, "p-md-t")]/span[contains(.," 19") or contains(.," 20")]/text()')
+	seller = listing.xpath('//a[text()="Informazioni sul venditore"]/@href')[0] or ''
+	title = listing.xpath('//div[@id="book-1"]//h2/a[@itemprop="url"]/@title')[0] or ''
+	description = listing.xpath('//div[@id="book-1"]//p[contains(@class, "p-md-t")]/span[contains(.," 19") or contains(.," 20")]/text()') or ''
+	notes = ''
+	year = ''
 	if description:
 		year = re.findall(r'\b(?:19|20)[0-9]{2}\b', description[0])[0]
 	if 'Anybook' in seller:
 		notes = listing.xpath('//div[@id="book-1"]//p[contains(@class, "p-md-t")]/text()')[0].strip().replace('Codice articolo ', '')
-	writer.writerow([code, seller, price, title, year, notes])
+	return [isbn, seller, price, title, year, notes]
 
-def printLibraccio(isbn):
-	listing = html.fromstring( s.get('http://libraccio.it/libro/%s/' % code ).text )
+def getAlibris(isbn):
+	sleep(4)
+	alibris = t.get('https://www.alibris.com/search/books/isbn/%s' % isbn , timeout=10).text
+	if 'Enter Your Search Terms' in alibris:
+		return
+	listing = html.fromstring(alibris)
+	price = listing.xpath('//td[@class="price"]/p/text()')[0]
+	#seller = listing.xpath('//td[@class="seller"]//a[@class="seller-link"]/text()')[0]
+	#title = listing.xpath('//h1/[@itemprop="name"]/text()')
+	#description = listing.xpath('//p[@id="synopsis-limited"]/text()')
+	#year = listing.xpath('//span[@itemprop="datePublished"]/text()')
+	return [isbn, '', price.strip(), '', '', '']
+
+def getLibraccio(isbn):
+	listing = html.fromstring( s.get('http://libraccio.it/libro/%s/' % isbn).text )
 	price = listing.xpath('//span[@class="currentprice" and @id="C_C_ProductDetail_lSellPriceU"]/text()')[0]
-	writer.writerow([code, u'Libraccio', price, u''])
+	return [isbn, u'Libraccio', price, u'']
 
-for code in wishlist.readlines():
-	code = code.strip()
-	print code
-	notes = ''
-	year = ''
-	try:
-		printAbebooks(code)
-	except IndexError:
-		print("NOTICE: not found")
-	except requests.exceptions.ConnectionError:
-		print("WARNING: Connection error. Sleeping.")
-		sleep(5)
-	except requests.exceptions.ReadTimeout:
-		print("WARNING: Connection timeout. Sleeping.")
-		sleep(15)
-	except:
-		print("ERROR: Unexpected exception")
-		pass
+def main():
+	wishlist = open('wishlist.txt', 'rb')
+	offerings = open('wishlist-offerings.csv', 'a')
 
-wishlist.close()
-offerings.close()
+	writer = csv.writer(offerings,
+			delimiter=b'\t',
+			lineterminator='\n',
+			quoting=csv.QUOTE_MINIMAL,
+			encoding='utf-8'
+			)
+	writer.writerow([u'ISBN', u'Bookshop', u'Price', u'Title', u'Year', 'Notes'])
+
+	for code in wishlist.readlines():
+		code = code.strip()
+		print code
+		try:
+			offer = getAbebooks(code)
+			if offer:
+				writer.writerow(offer)
+			else:
+				print "Not found"
+		except IndexError:
+			print("NOTICE: not found")
+		except requests.exceptions.ConnectionError:
+			print("WARNING: Connection error. Sleeping.")
+			sleep(5)
+		except requests.exceptions.ReadTimeout:
+			print("WARNING: Connection timeout. Sleeping.")
+			sleep(15)
+		#except:
+		#	print("ERROR: Unexpected exception")
+		#	pass
+
+	wishlist.close()
+	offerings.close()
+
+if __name__ == "__main__":
+	main()
