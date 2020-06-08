4fc87321b14fbc69eefd1de578622f488e66befa
Federico Leva <federicoleva@tiscali.it>
2020-06-09 00:12:45 +0300

    Get more offerings for the IA wishlist
    * Python 3 support.
    * Parse random strings (e.g. from Italian Wikipedia references).
    * Newer HTML parsing for AbeBooks.

--- ia-wishlist-offerings.py
+++ ia-wishlist-offerings.py
@@ -9,21 +9,45 @@
 __version__ = '0.1.2'
 
 import requests
+try:
+    from isbnlib import is_isbn10, is_isbn13, isbn_from_words
+except ImportError:
+    print("WARNING: No isbnlib, cannot query ISBNS")
 from lxml import html
 import re
 from time import sleep
-import unicodecsv as csv
+try:
+	import unicodecsv as csv
+except ImportError:
+	import csv
 s = requests.Session()
 t = requests.Session()
+#headers = {'user-agent': '', 'cookie': ''}
 
-def getAbebooks(isbn):
-	listing = html.fromstring( t.get('http://www.abebooks.it/servlet/SearchResults?isbn=%s&sortby=2&n=200000237' % isbn , timeout=10).text )
+def getAbebooks(isbn, fulltitle=None, keywords=None):
+	notes = ''
+	year = ''
+
+	if isbn:
+		url = 'https://www.abebooks.it/servlet/SearchResults?isbn={}&sortby=2'.format(isbn)
+	elif fulltitle:
+		url = 'https://www.abebooks.it/servlet/SearchResults?an={}&pn={}&tn={}&yrl={}&sortby=2'.format(
+			fulltitle['authors'], fulltitle['publisher'], fulltitle['title'], fulltitle['year'])
+		year = fulltitle['year']
+	elif keywords:
+		url = 'https://www.abebooks.it/servlet/SearchResults?kn={}&sortby=2'.format(keywords)
+	else:
+		return
+	r = t.get(url, timeout=10, headers=headers).text
+
+	listing = html.fromstring( r )
 	price = listing.xpath('//div[@id="srp-item-price-1"]/text()')[0]
 	seller = listing.xpath('//a[text()="Informazioni sul venditore"]/@href')[0] or ''
 	title = listing.xpath('//div[@id="book-1"]//h2/a[@itemprop="url"]/@title')[0] or ''
 	description = listing.xpath('//div[@id="book-1"]//p[contains(@class, "p-md-t")]/span[contains(.," 19") or contains(.," 20")]/text()') or ''
-	notes = ''
-	year = ''
+
+	if not isbn:
+		isbn = listing.xpath('//div[@id="book-1"]//p[contains(@class, "isbn")]//a/@title')[-1]
 	if description:
 		year = re.findall(r'\b(?:19|20)[0-9]{2}\b', description[0])[0]
 	if 'Anybook' in seller:
@@ -48,29 +72,60 @@ def getLibraccio(isbn):
 	price = listing.xpath('//span[@class="currentprice" and @id="C_C_ProductDetail_lSellPriceU"]/text()')[0]
 	return [isbn, u'Libraccio', price, u'']
 
+def parseItalianCitation(wikitext):
+	# Simplistic parser to get just some data
+	# TODO: https://github.com/dissemin/wikiciteparser/issues/2
+	try:
+		citation = {
+			'title': ' '.join(re.findall('titolo *= *([^|}]+)', wikitext, flags=re.I)),
+			'authors': ' '.join(re.findall('(?:nome|cognome|autore|curatore) *= *([^|}]+)', wikitext, flags=re.I)),
+			'publisher': ' '.join(re.findall('editore *= *([^|}]+)', wikitext, flags=re.I)),
+			'year': ''.join(re.findall('anno *= *([^|}]+)', wikitext, flags=re.I)).strip(),
+		}
+		if len(''.join(citation.values())) > 10:
+			return citation
+		else:
+			return False
+	except IndexError:
+		return None
+
 def main():
-	wishlist = open('wishlist.txt', 'rb')
+	wishlist = open('wishlist.txt', 'r')
 	offerings = open('wishlist-offerings.csv', 'a')
 
 	writer = csv.writer(offerings,
-			delimiter=b'\t',
+			delimiter='\t',
 			lineterminator='\n',
 			quoting=csv.QUOTE_MINIMAL,
-			encoding='utf-8'
 			)
 	writer.writerow([u'ISBN', u'Bookshop', u'Price', u'Title', u'Year', 'Notes'])
 
 	for code in wishlist.readlines():
 		code = code.strip()
-		print code
+		keywords = None
+		if is_isbn10(code) or is_isbn13(code):
+			isbn = code
+			fulltitle = None
+		else:
+			isbn = None
+			fulltitle = parseItalianCitation(re.sub('[[\]]', '', code))
+			print(fulltitle)
+			keywords = ' '.join(re.findall('\| *(?:\w+ *= *)?([^|}]+)', re.sub('[\[\]]', '', code)))
+			if not fulltitle:
+				print(keywords)
+
 		try:
-			offer = getAbebooks(code)
+			offer = getAbebooks(isbn, fulltitle, keywords)
+			if keywords and not offer:
+				offer = getAbebooks(None, None, keywords)
 			if offer:
 				writer.writerow(offer)
+				print("INFO: Found {}".format(offer[0]))
 			else:
-				print "Not found"
+				print("NOTICE: Not found")
+			sleep(0.1)
 		except IndexError:
-			print("NOTICE: not found")
+			print("NOTICE: Not found")
 		except requests.exceptions.ConnectionError:
 			print("WARNING: Connection error. Sleeping.")
 			sleep(5)
